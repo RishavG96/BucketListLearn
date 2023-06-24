6ae34a94baea8ae53db78a27b560e40b8c0e90b6
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-24 18:51:07 +0530

    Initial Commit

---
+++ BucketListLearn/BucketListLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListLearnApp.swift
+//  BucketListLearn
+//
+//  Created by Rishav Gupta on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
